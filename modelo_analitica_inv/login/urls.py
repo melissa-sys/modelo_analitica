f4b0812cfdc11476d16878f74702991bba3b4da8
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2023-06-03 14:11:06 -0500

    App creation: login. Setup and  templates for login view

---
+++ modelo_analitica_inv/login/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import LoginIndexView
+# from .views import AILoginView
+# from .views import AILogoutView
+
+app_name = 'login'
+urlpatterns = [
+    path('login', LoginIndexView.as_view(), name='home'),
+    # path('', AILoginView.as_view(), name='index'),
+    # path('logout', AILogoutView.as_view(), name='goodbye')
+
+]
--- modelo_analitica_inv/modelo_analitica_inv/settings.py
+++ modelo_analitica_inv/modelo_analitica_inv/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Analitica apps
+    # Django native apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'modelo_analitica_inv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth redirect
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = 'home/'
+LOGOUT_REDIRECT_URL = '/'
