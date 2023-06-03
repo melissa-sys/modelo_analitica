990c02d553ca6de1917528e4c4e585b2ba1354ba
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2023-06-03 11:13:24 -0500

    Django project 'modelo_analitica_inv' start

---
+++ modelo_analitica_inv/modelo_analitica_inv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for modelo_analitica_inv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelo_analitica_inv.settings')
+
+application = get_asgi_application()
