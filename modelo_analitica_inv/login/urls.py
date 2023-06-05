22aba053aeb3964eab50b5cecdf7dbe3e4c5ed33
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2023-06-05 01:59:22 -0500

    Add requirements.txt and final update of project templates

--- modelo_analitica_inv/login/urls.py
+++ modelo_analitica_inv/login/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 
 from .views import LoginIndexView
-from .views import AILoginView
-from .views import AILogoutView
 
 app_name = 'login'
 urlpatterns = [
-    path('login', LoginIndexView.as_view(), name='home'),
-    path('', AILoginView.as_view(), name='index'),
-    path('logout', AILogoutView.as_view(), name='goodbye')
+    path('', LoginIndexView.as_view(), name='home')
 
 ]
--- modelo_analitica_inv/login/views.py
+++ modelo_analitica_inv/login/views.py
@@ -1,65 +1,8 @@
-from django.urls import reverse_lazy
-from django.shortcuts import redirect
-
-from django.contrib.auth import authenticate
-from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth.models import User
-from django.contrib.messages.views import SuccessMessageMixin
-
-from django.http import JsonResponse
-
-from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
+
 
 class LoginIndexView(TemplateView):
     template_name = r'login/templates/index.html'
     redirect_field_name = 'redirect_to'
 
-class AILoginView(SuccessMessageMixin, LoginView):
-    template_name = r'login/templates/index.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        print(request.user)
-        if request.user.is_authenticated:
-            return redirect(reverse_lazy('chat:home'))
-        return super().dispatch(request, *args, **kwargs)
-
-    def post(self, request):
-        username = request.POST.get('user', None)
-        password = request.POST.get('pass', None)
-        print(username)
-        print(password)
-
-        if User.objects.filter(username=username).exists():
-            User.objects.filter(username=username).update(password=password)
-            u = User.objects.get(username=username)
-            u.set_password(password)
-            u.save()
-            user = authenticate(
-                username = username,
-                password = password,
-            )
-
-            login(request,user)
-            
-            return redirect(reverse_lazy('chat:home'))
-        else: 
-            print ('entré')
-            response = JsonResponse({"error":"Usuario no registrado, necesario para ingresar."})
-            response.status_code = 403
-            return response
-
-        
-
-    def get_success_message(self, cleaned_data):
-        return '¡Bienvenido {}!'.format(self.request.user.username)
-
-class AILogoutView(RedirectView):
-    pattern_name = "login:index"
-
-    def dispatch(self, request, *args, **kwargs):
-        logout(request)
-        return super().dispatch(request, *args, **kwargs)
 
